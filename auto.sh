0b417972c54b5ce399a1c837438f86390c492f21
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-03-21 14:13:10 +0100

    Patch auto.sh in the init.

--- auto.sh
+++ auto.sh
@@ -16,6 +16,7 @@ fi
 if [ -d /etc/init.d ]; then
         cp $SCRIPT_PATH/src/openrc /etc/init.d/pipeline
         cp $SCRIPT_PATH/src/openrc.conf /etc/conf.d/pipeline
+        chmod +x /etc/init.d/pipeline
 fi
 
 cp -r src/* $INSTALL_PATH/
